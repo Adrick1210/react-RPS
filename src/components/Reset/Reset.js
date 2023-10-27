74f060ca12052bf2b38b979410068cb1a6153d86
Christian Alexander <christianalexander@christians-mbp.lan>
2023-10-26 18:21:04 -0700

    reset button function fixed

--- src/components/Reset/Reset.js
+++ src/components/Reset/Reset.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Reset.css";
 
-function Reset({ setPlayerScore, setComputerScore, setPlayerChoice, setComputerChoice }) {
+function Reset({ setPlayerScore, setComputerScore, setPlayerChoice, setComputerChoice, setWhoWon }) {
   function resetGame() {
     setComputerScore(0);
     setPlayerScore(0);
     setComputerChoice("");
     setPlayerChoice("");
+    setWhoWon("")
   }
   return <button onClick={resetGame}>Reset</button>;
 }
